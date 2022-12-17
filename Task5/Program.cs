e284415d72f80f83e1488646cf608eed40a594db
anna.v.glazunova@yandex.ru <AnnaGlazunova>
2022-12-17 15:57:14 +0300

    Task 5 done

---
+++ Task5/Program.cs
@@ -0,0 +1,8 @@
+﻿// 5. Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Enter 3-digit number:");
+int a = int.Parse(Console.ReadLine()!);
+int b = a/10;
+int last_fig = a - b * 10;
+Console.Write("Last figure of the number:");
+Console.WriteLine(last_fig);
