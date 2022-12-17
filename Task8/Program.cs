f2332aab9673c8bbe0206fec959fbf4dafd812da
anna.v.glazunova@yandex.ru <AnnaGlazunova>
2022-12-17 16:29:10 +0300

    Task 8 done

---
+++ Task8/Program.cs
@@ -0,0 +1,8 @@
+﻿// 8. Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Enter 3-digit number:");
+int a = int.Parse(Console.ReadLine()!);
+int b = a/100;
+int c = a/10;
+int del = b*10 + a - c*10;
+    Console.Write("Result:"); 
+    Console.Write(del);
