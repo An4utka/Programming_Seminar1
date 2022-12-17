f6ee2e4c60d8c19256d117cf7de55893a1f585dd
anna.v.glazunova@yandex.ru <AnnaGlazunova>
2022-12-17 16:31:31 +0300

    Task 1 done

---
+++ Task1_second_try/Program.cs
@@ -0,0 +1,16 @@
+﻿// 1. По двум заданным числам проверять является ли первое квадратом второго
+
+
+Console.WriteLine("Enter first number");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter second number");
+int b = int.Parse(Console.ReadLine()!);
+
+if (b == a * a)
+{
+    Console.Write("Yes");
+}
+else 
+{
+    Console.Write("No");
+}
