ed78e572373432e61da7a709b9451ebced7d0c3b
anna.v.glazunova@yandex.ru <AnnaGlazunova>
2022-12-17 15:45:33 +0300

    Task 4 done

---
+++ Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿// 4. Выяснить является ли число чётным
+int a = new Random().Next(1,100);
+Console.Write("a=");
+Console.WriteLine(a);
+if (a%2==0)
+{
+    Console.Write("Even");
+}
+else Console.Write ("Uneven");
