15594c071b20ab6123af3472d626fee2d4e1557d
anna.v.glazunova@yandex.ru <AnnaGlazunova>
2022-12-17 15:38:31 +0300

    Task 3 done

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// 3. Найти максимальное из трех чисел
+int a = new Random().Next(1,100);
+int b = new Random().Next(1,100);
+int c = new Random().Next(1,100);
+int max = a;
+Console.Write("a=");
+Console.WriteLine(a);
+Console.Write("b=");
+Console.WriteLine(b);
+Console.Write("c=");
+Console.WriteLine(c);
+
+if (b>max)
+{
+    max = b;
+}
+if (c>max)
+{
+    max = c;
+}
+Console.Write("Max=");
+Console.WriteLine(max);
