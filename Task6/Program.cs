95933d378d75069ebe9b024af6210f7252fa9db1
anna.v.glazunova@yandex.ru <AnnaGlazunova>
2022-12-17 16:10:45 +0300

    Task 6 done

---
+++ Task6/Program.cs
@@ -0,0 +1,8 @@
+﻿// 6. Показать вторую цифру трёхзначного числа
+Console.WriteLine("Enter 3-digit number:");
+int a = int.Parse(Console.ReadLine()!);
+int b = a/10;
+int c = a/100;
+int middle = b - c*10;
+Console.Write("Middle figure of the number:");
+Console.WriteLine(middle);
